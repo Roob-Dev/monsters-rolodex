25efcde905ea1f156352d61af5eed5f807e81304
rubi-gallegos <rubi.g.llam@gmail.com>
2021-11-16 18:51:42 -0600

    card list with monster images

--- src/App.js
+++ src/App.js
@@ -18,9 +18,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,7 +1,12 @@
+import Card from "../card/card.component";
 import "./card-list.styles.css";
 
-const CardList = (props) => {
-  return <div className="card-list">{props.children}</div>;
-};
+const CardList = (props) => (
+  <div className="card-list">
+    {props.monsters.map((monster) => (
+      <Card key={monster.id} monster={monster} />
+    ))}
+  </div>
+);
 
 export default CardList;
